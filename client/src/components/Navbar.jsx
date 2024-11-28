5cd6e99e93921dbb6beef5e9be6e8775c4731cf7
W Rivera <wrivera@Ws-iMac.fios-router.home>
2024-11-28 18:05:23 -0500

    added z

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ export default function Navbar() {
           className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-green-500 hover:bg-green-600 text-white h-9 rounded-md px-3"
           to="/create"
         >
-          Create Employee
+          Create Employeez
         </NavLink>
       </nav>
     </div>
